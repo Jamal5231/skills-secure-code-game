dc4da71ec528fa66d83319bcc4ee12d81c9f7ac1
Jamal5231 <thales523@gmail.com>
2025-04-26 17:56:55 -0300

    Update code.py

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -26,7 +26,7 @@ def validorder(order: Order):
         if item.type == 'payment':
             net += item.amount
         elif item.type == 'product' and:
-                net -= item.amount * item.quantity    
+            net -= item.amount * item.quantity    
         else:
             return "Invalid item type: %s" % item.type
 
